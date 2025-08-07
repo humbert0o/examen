b521bd304dfb435694223fe59b020eeef9fff868
humbert0o <humbertoalvarezd07@gmail.com>
2025-08-06 21:17:41 -0400

    ejercicio5.js

--- ejercicio5.js
+++ ejercicio5.js
@@ -1,1 +1,8 @@
-const edad= Number(prompt("Ingresa tu edad": ))
+const edad = Number(prompt("Ingrese su edad: "));
+if (edad >= 18 && edad <= 64) {
+    console.log(true); 
+    console.log("Eres apto para trabajar");
+} else {
+    console.log(false);
+    console.log("No eres apto para trabajar");
+}
