d3ca964544aa15e095d75e9d4433a5cb496f7a2c
humbert0o <humbertoalvarezd07@gmail.com>
2025-08-08 14:14:50 -0400

    Add files via upload

---
+++ ejercicio_sql.sql
@@ -0,0 +1,24 @@
+create table Estudiantes(
+    id int PRIMARY KEY, 
+    nombre VARCHAR(50), 
+    apellido VARCHAR(50)
+); 
+INSERT INTO Estudiantes (id, nombre, apellido) 
+
+VALUES 
+    (16186354, "Roberto", "Borges"),
+    (29767741, "Jinmy", "Chacin"), 
+    (30604870, "Maria", "Urdaneta"),
+    (30820000, "Daniel", "Sandoval"),
+    (31279773, "Daviannys", "Villalobos"), 
+    (31913070, "Humberto", "Alvarez"),
+    (32056687, "German", "Suarez"), 
+    (32410084, "Julio", "Pacheco"), 
+    (32636138, "Juan", "Polania"),
+    (32698769, "Jose", "Velazques"), 
+    (33319114, "Gabriel", "Suarez"), 
+    (33607751, "Robert", "Boscan"), 
+    (33893333, "Carlos", "Moreno"), 
+    (33966984, "Jimn", "Chacin"),
+    (34544638, "Juan", "Arrieta");
+    
